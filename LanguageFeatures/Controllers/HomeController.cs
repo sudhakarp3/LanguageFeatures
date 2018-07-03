848bda7f809af09b6c2832586cdc6397b10aac0b
Sudhakar <sudha2u@gmail.com>
2018-07-03 04:36:32 +0400

    lang features

---
+++ LanguageFeatures/Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using LanguageFeatures.Models;
+
+namespace LanguageFeatures.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public String Index()
+        {
+            return "Navigate to a URL to show an example"; 
+        }
+        public ViewResult AutoProperty()
+        {
+            Product myProduct = new Product();
+            myProduct.Name = "Test prod";
+            String strProdName = myProduct.Name;
+            return View("Result",(object)String.Format("Product Name{0}",strProdName));
+        }
+        public ViewResult CreateProduct()
+        {
+            Product myProduct = new Product();
+            myProduct.ProductID = 100;
+            myProduct.Name = "Kayak";
+            myProduct.Description = "A boat for one person";
+            myProduct.Price = 275M;
+            myProduct.Category = "Watersports";
+            return View("Result",(object)String.Format("Category: {0}", myProduct.Category));
+            
+
+        }
+        public ViewResult CreateProductObjectInitializer()
+        {
+            Product myProduct = new Product { ProductID=100,Name="kkk",Description="check",Price=22,Category="wsports"};
+            
+            return View("Result", (object)String.Format("Category: {0}", myProduct.Category));
+
+
+        }
+    }
+}
---
+++ LanguageFeatures/Models/Product.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LanguageFeatures.Models
+{
+    public class Product
+    {
+        public int ProductID { get; set; }
+        public string name { get; set; }
+        public string Name
+        {
+            get
+            {
+                return ProductID + name;
+            }
+            set
+            {
+                name = value;
+            }
+        }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public string Category { set; get; }
+    }
+}
