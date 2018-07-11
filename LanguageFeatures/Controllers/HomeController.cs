9d206c5cf70ba916783e94bf88ef2c4316df0a44
Sudhakar <sudha2u@gmail.com>
2018-07-11 15:22:03 +0400

    Performing Language Integrated Queries

--- LanguageFeatures/Controllers/HomeController.cs
+++ LanguageFeatures/Controllers/HomeController.cs
@@ -178,9 +178,60 @@ namespace LanguageFeatures.Controllers
             }
             return View("Result", (object)result.ToString());
         }
-    
+        //usual way to find top 3 products
+        /*public ViewResult FindProducts()
+        {
+            Product[] products = {
+new Product {Name = "Kayak", Category = "Watersports", Price = 275M},
+new Product {Name = "Lifejacket", Category = "Watersports", Price = 48.95M},
+new Product {Name = "Soccer ball", Category = "Soccer", Price = 19.50M},
+new Product {Name = "Corner flag", Category = "Soccer", Price = 34.95M}
+};
+            // define the array to hold the results
+            Product[] foundProducts = new Product[3];
+            // sort the contents of the array
+            Array.Sort(products, (item1, item2) => {
+                return Comparer<decimal>.Default.Compare(item1.Price, item2.Price);
+            });
+            // get the first three items in the array as the results
+            Array.Copy(products, foundProducts, 3);
+            // create the result
+            StringBuilder result = new StringBuilder();
+            foreach (Product p in foundProducts)
+            {
+                result.AppendFormat("Price: {0} ", p.Price);
+            }
+            return View("Result", (object)result.ToString());
+        }*/
+        //Linq way to find top 3 products
+        //Performing Language Integrated Queries
+        public ViewResult FindProducts()
+        {
+            Product[] products = {
+new Product {Name = "Kayak", Category = "Watersports", Price = 275M},
+new Product {Name = "Lifejacket", Category = "Watersports", Price = 48.95M},
+new Product {Name = "Soccer ball", Category = "Soccer", Price = 19.50M},
+new Product {Name = "Corner flag", Category = "Soccer", Price = 34.95M}
+};
+            var foundProducts = from match in products
+                                orderby match.Price descending
+                                select new { match.Name, match.Price };
+            // create the result
+            int count = 0;
+            StringBuilder result = new StringBuilder();
+            foreach (var p in foundProducts)
+            {
+                result.AppendFormat("Price: {0} ", p.Price);
+                if (++count == 3)
+                {
+                    break;
+                }
+            }
+            return View("Result", (object)result.ToString());
+        }
 
-    }
+
+        }
 } 
 
 
